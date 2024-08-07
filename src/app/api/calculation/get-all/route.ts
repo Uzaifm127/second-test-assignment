60bdcebd95ef97b2f460dfcdbe36866610177557
Uzaifm127 <uzaifm127@gmail.com>
2024-08-07 17:05:20 +0530

    removed the headers in the next response

--- src/app/api/calculation/get-all/route.ts
+++ src/app/api/calculation/get-all/route.ts
@@ -11,17 +11,10 @@ export const GET = async () => {
       prisma.calculationReply.findMany(),
     ]);
 
-    return NextResponse.json(
-      {
-        calculationPost,
-        calculationReply,
-      },
-      {
-        headers: {
-          "Cache-Control": "no-store, must-revalidate",
-        },
-      }
-    );
+    return NextResponse.json({
+      calculationPost,
+      calculationReply,
+    });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 400 });
   }
